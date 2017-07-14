833c46102fe642073cd404c78622453d25d2b5b1
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 01:46:55 +0530

    Adding back the normal code for phase var

--- views/js/main.js
+++ views/js/main.js
@@ -495,13 +495,14 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var phase = Math.sin((document.body.scrollTop / 1250));
+  //var phase = Math.sin((document.body.scrollTop / 1250));
   for (var i = 0; i < items.length; i++) {
 
     /*Here, first the style segment is changed and then the layout.*/
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     /*There was no need of modulo operator as the result was i.*/
-     phase+=i /*(i % 5)*/;
+    var phase = Math.sin((document.body.scrollTop / 1250)+phase+i);
+     
     console.log(phase);
   }
 
