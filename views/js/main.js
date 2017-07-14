89bdb1e6cc45ba2352c9763d587ee2eedb107b28
radhieee123 <radhikadaxini001@gmail.com>
2017-07-14 14:54:45 +0530

    UdacityLesson_changesin main.js

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var random=document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < random.length; i++) {
+      var dx = determineDx(random[i], size);
+      var newwidth=0;
+      random[i].style.width = newwidth+"%";
     }
   }
 
