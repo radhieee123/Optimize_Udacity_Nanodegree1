7fa6faefe86fb92b55e9cc1b4a895257a208279d
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 11:53:33 +0530

    from100 to 50

--- views/js/main.js
+++ views/js/main.js
@@ -568,7 +568,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 50; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
