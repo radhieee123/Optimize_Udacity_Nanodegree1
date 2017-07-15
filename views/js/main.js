e5c948ab06f18d1b2a40e805fda4e9928edd6c4e
radhieee123 <radhikadaxini001@gmail.com>
2017-07-16 01:16:57 +0530

    movingPizzas in main.js

--- views/js/main.js
+++ views/js/main.js
@@ -543,6 +543,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
     var cols = 8;
     var s = 256;
+    var movingPizzas = document.getElementById('movingPizzas1');
     for (var i = 0,elem; i < 24; i++) {
         elem = document.createElement('img');
         elem.className = 'mover';
@@ -551,7 +552,8 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
-        document.querySelector("#movingPizzas1").appendChild(elem);
+        movingPizzas.appendChild(elem);
+
     }
     updatePositions();
 });
