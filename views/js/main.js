d3a66887386bdc3b75591707a7ca01379d6d5491
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 12:31:33 +0530

    iteration 80

--- views/js/main.js
+++ views/js/main.js
@@ -568,7 +568,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var elem = document.createElement('img');
-  for (var i = 0; i < 30; i++) {    
+  for (var i = 0; i < 80; i++) {    
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
