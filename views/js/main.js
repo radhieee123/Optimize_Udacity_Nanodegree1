a336773278083e5893814534982df5af98ac1a9c
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 12:32:21 +0530

    5 Undo

--- views/js/main.js
+++ views/js/main.js
@@ -567,8 +567,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var elem = document.createElement('img');
-  for (var i = 0; i < 80; i++) {    
+  for (var i = 0; i < 80; i++) {
+    var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
