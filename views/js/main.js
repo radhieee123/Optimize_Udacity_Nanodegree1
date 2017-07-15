32119f4a8b7f1ff8dd4051316dbe3fd180c25b19
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 11:55:50 +0530

    p down to .mover class

--- views/js/main.js
+++ views/js/main.js
@@ -542,8 +542,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-  var p=(document.body.scrollTop);
   var items = document.querySelectorAll('.mover');
+    var p=(document.body.scrollTop);
+
   for (var i = 0; i < items.length; i++) {
     //var phase = Math.sin((p) + (i));
     items[i].style.left = items[i].basicLeft + 100 * (Math.sin((p/1250) + (i))) + 'px';
