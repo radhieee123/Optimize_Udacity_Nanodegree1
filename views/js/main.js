802dc1c36f0e5bb0b6c5948b95e9d2b11dc7efbb
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 10:09:00 +0530

    timetochange position var

--- views/js/main.js
+++ views/js/main.js
@@ -555,10 +555,10 @@ function updatePositions() {
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
- // var t= window.performance.getEntriesByName("measure_frame_duration");
+ var t= window.performance.getEntriesByName("measure_frame_duration");
   if (frame % 10 === 0) {
-    //var timesToUpdatePosition = t
-    logAverageFrame(window.performance.getEntriesByName("measure_frame_duration"));
+    var timesToUpdatePosition = t
+    logAverageFrame(timesToUpdatePosition);
   }
 }
 
