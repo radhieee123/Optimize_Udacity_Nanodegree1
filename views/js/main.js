5498d1df09ddfcbe1c4492e7cf36f906b90bd5e1
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 09:58:24 +0530

    var p in new func

--- views/js/main.js
+++ views/js/main.js
@@ -544,8 +544,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var p=document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((p / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
