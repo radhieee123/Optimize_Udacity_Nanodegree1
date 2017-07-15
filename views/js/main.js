31cb9b73fc9865efb55b491183e7f73ef9b01122
radhieee123 <radhikadaxini001@gmail.com>
2017-07-15 12:44:58 +0530

    elem var again outside for loop

--- views/js/main.js
+++ views/js/main.js
@@ -570,8 +570,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem = document.createElement('img');
   for (var i = 0; i < 100; i++) {
-    var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
